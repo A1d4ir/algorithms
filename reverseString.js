44078df844d1808e140239dd3bde040bcf25fe0f
Aldair <aldair_fime@hotmail.com>
2024-01-07 13:08:38 -0600

    Adding MyArray implemented in Javascript, mergeSortedArrays Problem with javascript and reverseString problem solution with javascript

---
+++ reverseString.js
@@ -0,0 +1,29 @@
+// Create a function that reverses a string
+//'Hi My name is Aldair' Should be
+// 'riadlA si eman yM iH'
+
+function reverse(str) {
+  //check input
+  if (!str || str.length < 2 || typeof str !== 'string') {
+    return 'hmm that is not good';
+  }
+
+  const backwards = [];
+  const totalItems = str.length - 1;
+  console.log(totalItems);
+  for (let i = totalItems; i >= 0; i--) {
+    backwards.push(str[i]);
+  }
+
+  console.log(backwards);
+
+  return backwards.join('');
+}
+
+function reverse2(str) {
+  return str.split('').reverse().join('');
+}
+
+const reverse3 = str => [...str].reverse().join('');
+
+console.log(reverse3('Hi My name is Aldair'));
