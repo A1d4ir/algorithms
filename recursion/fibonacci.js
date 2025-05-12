e5dd5f56eb3679ffce435a87427fb8ac64725e91
A1d4ir <aldair_fime@hotmail.com>
2025-05-11 18:44:15 -0600

    adding fibonacci recursion and iterative example

---
+++ recursion/fibonacci.js
@@ -0,0 +1,21 @@
+function fibonacciRecursive(n) {
+  if(n < 2) {
+    return n;
+  }
+
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+// console.log('fibonacciRecursive: ', fibonacciRecursive(50));
+
+function fibonacciIterative(n) {
+  let arr = [0, 1];
+
+  for (let i = 2; i < n + 1; i++) {
+    arr.push(arr[i -2] + arr[i - 1]);
+  }
+
+  return arr[n];
+}
+
+console.log('fibonacciIterative: ', fibonacciIterative(50));
